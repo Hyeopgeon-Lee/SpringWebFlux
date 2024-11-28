f7db88661c819eb90dfebdca2f27fb51cc49745f
Hyeopgeon Lee <soft1318@naver.com>
2024-11-28 15:38:45 +0900

    테스트 공부용 코드 추가

---
+++ src/test/java/kopo/poly/test/MonoFluxExample.java
@@ -0,0 +1,20 @@
+package kopo.poly.test;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class MonoFluxExample {
+
+    public static void main(String[] args) {
+        // 단일 데이터 생성 (Mono)
+        Mono<String> mono = Mono.just("User ID: 12345");
+
+        // 다수의 데이터 생성 (Flux)
+        Flux<String> flux = Flux.just("Order1", "Order2", "Order3");
+
+        // Mono와 Flux 조합
+        flux.flatMap(order -> mono.map(user -> user + " - " + order))
+                .subscribe(System.out::println);
+    }
+
+}
