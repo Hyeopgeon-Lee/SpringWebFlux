4ed53b8b20c46211591e2b50c0e24d16329268fb
Hyeopgeon Lee <soft1318@naver.com>
2024-11-28 15:00:38 +0900

    테스트 공부용 코드 추가

---
+++ src/test/java/kopo/poly/test/MapTest.java
@@ -0,0 +1,14 @@
+package kopo.poly.test;
+
+import reactor.core.publisher.Mono;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("Hello, WebFlux")
+                .map(data -> data.toUpperCase()) // 데이터를 대문자로 변환
+                .map(data -> "변환된 데이터: " + data); // 문자열 추가
+
+        mono.subscribe(System.out::println);
+
+    }
+}
---
+++ src/test/java/kopo/poly/test/MonoTest.java
@@ -0,0 +1,20 @@
+package kopo.poly.test;
+
+import reactor.core.publisher.Mono;
+
+public class MonoTest {
+
+    public static void main(String[] args) {
+
+        // Mono 정의 (데이터 생성 로직)
+        Mono<String> mono = Mono.defer(() -> {
+            System.out.println("데이터 생성 중...");
+            return Mono.just("Hello, WebFlux!");
+        });
+
+        System.out.println("Mono 정의 완료");
+
+        // 구독 시점 (이 시점에만 데이터 생성 실행)
+        mono.subscribe(data -> System.out.println("데이터 수신: " + data));
+    }
+}
