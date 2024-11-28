6ff2a0ff3a72756e38b2548eb0ffa86b08547968
Hyeopgeon Lee <soft1318@naver.com>
2024-11-28 15:25:02 +0900

    테스트 공부용 코드 추가

--- src/test/java/kopo/poly/test/FlatmapTest.java
+++ src/test/java/kopo/poly/test/FlatmapTest.java
@@ -1,15 +1,22 @@
 package kopo.poly.test;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class FlatmapTest {
 
     public static void main(String[] args) {
-        Mono<String> mono = Mono.just("Hello, WebFlux")
-                .map(data -> data.toUpperCase()) // 데이터를 대문자로 변환
-                .map(data -> "변환된 데이터: " + data); // 문자열 추가
+        Flux<String> flux = Flux.just("User1", "User2", "User3")
+                .flatMap(userId -> {
+                    // 비동기 작업: 사용자 이름 가져오기
+                    return Mono.just(userId + " - 학생들");
+                })
+                .map(userInfo -> {
+                    // 데이터 변환: 대문자로 변경
+                    return userInfo.toUpperCase();
+                });
 
-        mono.subscribe(System.out::println);
+        flux.subscribe(System.out::println);
 
     }
 }
